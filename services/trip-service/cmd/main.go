c6ec890ac3490103e9199f120afc37fab348f5f8
Ritik kumar <ritikbaliyanpra123@gmail.com>
2026-01-26 18:09:36 +0000

    Added graceful shutdown to servers

--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -1,19 +1,55 @@
-package cmd
+package main
 
 import (
 	"context"
-	"ride-sharing/services/trip-service/internal/domain"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	h "ride-sharing/services/trip-service/internal/infrastructure/http"
 	"ride-sharing/services/trip-service/internal/infrastructure/repository"
 	"ride-sharing/services/trip-service/internal/service"
+	"syscall"
+	"time"
 )
 
 func main() {
-	ctx := context.Background()
 	inmemRepo := repository.NewInmemRepository()
 	svc := service.NewService(inmemRepo)
-	fare := &domain.RideFareModel{
-		UserID: "42",
-	}
-	svc.CreateTrip(ctx, fare)
+	mux := http.NewServeMux()
 
+	httphandler := h.HttpHandler{Service: svc}
+
+	mux.HandleFunc("POST /preview", httphandler.HandleTripPreview)
+
+	server := &http.Server{
+		Addr:    ":8083",
+		Handler: mux,
+	}
+
+	serverErrors := make(chan error, 1)
+
+	go func() {
+		log.Printf("Server listening on %s", server.Addr)
+		serverErrors <- server.ListenAndServe()
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err := <-serverErrors:
+		log.Printf("Error starting server: %v", err)
+
+	case sig := <-shutdown:
+		log.Printf("Server is shutting down due to %v signal", sig)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil {
+			log.Printf("Could not stop server gracefully: %v", err)
+			server.Close()
+		}
+	}
 }
